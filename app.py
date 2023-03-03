5e3a7ea2a1aeaf522d5a69dfb751dd30d7e6fd7e
dtt2307 <62041478+dtt2307@users.noreply.github.com>
2023-03-03 10:44:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1 style='text-align: center;'>This is my final project of the Cloud DevOps Engineer course!</h1>"
+    return "f<h1 style='text-align: center;'>This is my final project of the Cloud DevOps Engineer course!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
